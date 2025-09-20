763d575292cfc5010070e6008d6d3fe4e25f9b5e
Raham Kalhoro <rahamkalhoro@gamil.com>
2025-09-20 18:24:59 +0500

    added new features

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let removeBtn = document.querySelector("removeBtn1");
 removeBtn.addEventListener("click", () => {
   removeBtn.style.background = "red";
 });
+// add a toggle button which switch two conditions
